c6940b044fa5b5da29feb4c54ae7f6f7ddfd6032
AlizPhucs17 <hongphuc_0107@yahoo.com>
2023-12-21 23:54:03 +0700

    .

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   if (pathname == '/') {
-    return NextResponse.redirect(new URL('/feed', req.url));
+    return NextResponse.redirect(new URL('/auth/sign-in', req.url));
   }
 
   return NextResponse.next();
